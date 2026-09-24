427ba6816a7176121b83d2c0ab4cc4caa0c3d4ed
Vassa Zhuk <zhookvasilisad@gmail.com>
2026-09-24 13:39:51 +0300

    lab: реализация. нет проверки на deadlock и livelock

---
+++ src/main/java/org/labs/DinningRoom.java
@@ -0,0 +1,24 @@
+package org.labs;
+
+public class DinningRoom {
+    public static void main(String[] args) throws Exception {
+        Programmer[] programmers = new Programmer[7];
+        Object[] spoons = new Object[programmers.length];
+
+        for (int i = 0; i < spoons.length; i++){
+            spoons[i] = new Object();
+        }
+
+        for (int i = 0; i < programmers.length; i++){
+            Object leftSpoon = spoons[i];
+            Object rightSpoon = spoons[(i + 1) % spoons.length];
+
+            programmers[i] = new Programmer(leftSpoon, rightSpoon);
+
+            Thread t = new Thread(programmers[i], "Programmer " + (i + 1));
+            t.start();
+
+        }
+
+    }
+}
---
+++ src/main/java/org/labs/Programmer.java
@@ -0,0 +1,65 @@
+package org.labs;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Programmer implements  Runnable {
+
+    private static final AtomicInteger totalPortionsLeft = new AtomicInteger(100);
+    private Object leftSpoon;
+    private Object rightSpoon;
+
+    private static Semaphore waiters = new Semaphore(2, true);
+    public Programmer(Object leftSpoon, Object rightSpoon) {
+        this.leftSpoon = leftSpoon;
+        this.rightSpoon = rightSpoon;
+    }
+
+    private void doAction(String action) throws InterruptedException {
+        System.out.println(
+                Thread.currentThread().getName() + " " + action
+        );
+        Thread.sleep((int) (Math.random() * 100));
+    }
+
+    private void orderMeal() throws InterruptedException {
+        doAction(Thread.currentThread().getName() + ": Waiting for a waiter");
+        waiters.acquire();
+    }
+
+    private void takeMeal() throws InterruptedException {
+        waiters.release();
+        doAction(Thread.currentThread().getName() + ": Take meal from waiter");
+    }
+
+    @Override
+    public void run(){
+        try {
+            //tutorial
+            while (true) {
+                doAction(System.nanoTime() + ": Thinking");
+                if (totalPortionsLeft.get() <= 0) break;
+                orderMeal();
+                synchronized (leftSpoon) {
+                    doAction(System.nanoTime() + ": Picked left spoon");
+                    synchronized (rightSpoon) {
+                        int remaining = totalPortionsLeft.get();
+                        //if (remaining <= 0) break;
+                        if (remaining > 0 &&
+                                totalPortionsLeft.compareAndSet(remaining, remaining - 1)){
+                            doAction(System.nanoTime() + ": Picked right spoon - eating. Portions left: " + remaining);
+                            doAction(System.nanoTime() + ": Put down right spoon");
+                        }
+                    }
+                    doAction(System.nanoTime() + ": Put down left spoon. Back to thinking");
+                }
+                takeMeal();
+            }
+        } catch (InterruptedException e) {
+            //throw new RuntimeException(e);
+            Thread.currentThread().interrupt();
+            return;
+        }
+    }
+
+}
